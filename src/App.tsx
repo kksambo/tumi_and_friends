6425354cc7a72760d58b5894d13471a357876b84
POL-1-G1B-12 <sambokksicelo98@gmail.com>
2025-04-07 17:23:07 +0200

    Initial com

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Use createRoot from react-dom/client
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
@@ -9,27 +9,22 @@ import RegistrationForm from "./components/RegistrationForm.tsx";
 import DustbinInteraction from "./components/DustbinInteraction.tsx";
 import Profile from "./pages/Profile.tsx";
 
-import App from "./App.tsx";
-
-ReactDOM.render(
-  <BrowserRouter basename="/tumi_and_friends">
-    <MainApp />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
-
 function MainApp() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/tumi_and_friends">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/dustbininteraction" element={<DustbinInteraction />} />
-        <Route path="/profile" element={<Profile />} /> 
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+// Use ReactDOM.createRoot
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<MainApp />);
 
 export default MainApp;
